92c6a3f832300c095ae8eb4da29517b5583fc7f6
bhargav27b <bhargav@sketchbrahma.com>
2022-07-20 10:23:22 +0530

    delete history changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,58 +22,27 @@ for(var i = 0; i < btns.length; i++) {
     }            
 }
 
-var historyArray = [];
-
 equalBtn.onclick = (e) => {        
 
-    historyArray.push({result: eval(result)});
+    var tr = document.createElement('tr');
+    var resultTd = document.createElement('td');        
+    var buttonTd = document.createElement('td');        
+    var button = document.createElement('button');  
+    button.onclick = deleteBtn;      
 
-    var key = historyArray.key;
+    var tr = document.createElement('tr');
+    historyDiv.appendChild(tr).appendChild(resultTd).insertAdjacentElement("afterend", buttonTd).appendChild(button);                     
+    resultTd.innerText = result + ' = ' + eval(result);
+    button.className = 'deleteBtn';
+    button.innerHTML = '<i class="fas fa-times"></i>';
 
-    // historyArray.map((index, data) => {
-    //     sessionStorage.setItem(index, data.result);  
-    // })
-
-    console.log(historyArray);
-
-    var key = resultList.innerText;
-    // var value = resultList.innerText;
-
-    sessionStorage.setItem(key, JSON.stringify(historyArray));    
-    // sessionStorage.setItem(key, value);    
-    save(); 
+    function deleteBtn(e) {
+        e.preventDefault();
+        e.target.parentNode.parentNode.parentNode.style.display = "none";
+    }
 
     result = "";
     resultList.innerText = "";    
-    return false;
-}
-
-function save() {          
-
-    historyDiv.innerHTML = "";  
-    for(var i = 0; i < sessionStorage.length; i++) {
-        var a = sessionStorage.key(i);
-        var b = sessionStorage.getItem(a);
-
-        var tr = document.createElement('tr');
-        var resultTd = document.createElement('td');        
-        var buttonTd = document.createElement('td');        
-        var button = document.createElement('button');  
-        button.onclick = deleteBtn;      
-
-        var tr = document.createElement('tr');
-        historyDiv.appendChild(tr).appendChild(resultTd).insertAdjacentElement("afterend", buttonTd).appendChild(button);                     
-        resultTd.innerText = a + ' = ' + b.stringify;
-        button.className = 'deleteBtn';
-        button.innerHTML = '<i class="fas fa-times"></i>';
-
-        function deleteBtn(e) {
-            e.preventDefault();
-            sessionStorage.removeItem(a);
-            e.target.parentNode.parentNode.parentNode.style.display = "none";
-        }
-
-    }
     return false;
 }
 
